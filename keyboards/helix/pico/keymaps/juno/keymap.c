deca7b7fb4b2bf6b9608d58b6afc67509be5b9c8
Junya Ogura <juno@sooey.com>
2018-08-19 01:51:31 +0900

    Unassign "Next" and "Play" in Lower layer

--- keyboards/helix/pico/keymaps/juno/keymap.c
+++ keyboards/helix/pico/keymaps/juno/keymap.c
@@ -102,14 +102,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * |      |  F7  |  F8  |  F9  |  F10 |  F11 |             |  F12 |      | PrSc | Home | End  | Shift|
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |      | EISU |      | Next | Vol- | Vol+ | Play |
+   * |      |      |      |      |      |      |      |      | EISU |      |      | Vol- | Vol+ |      |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_LOWER] = LAYOUT( \
       KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
       _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,                    KC_F12,  _______, KC_PSCR, KC_HOME, KC_END,  KC_RSFT, \
-      _______, _______, _______, _______, _______, _______, _______, _______, KC_LANG2, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+      _______, _______, _______, _______, _______, _______, _______, _______, KC_LANG2, _______, _______, KC_VOLD, KC_VOLU, _______ \
       ),
 
   /* Raise
@@ -120,14 +120,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * |      |  F7  |  F8  |  F9  |  F10 |  F11 |             |  F12 |      | PrSc |PageDn|PageUp| Shift|
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * |      |      |      |      |      | KANA | Bksp |      |      |      | Next | Vol- | Vol+ | Play |
+   * |      |      |      |      |      | KANA | Bksp |      |      |      |      | Vol- | Vol+ |      |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_RAISE] = LAYOUT( \
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
       _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,                    KC_F12,  _______, KC_PSCR, KC_PGDN, KC_PGUP, KC_RSFT, \
-      _______, _______, _______, _______, _______, KC_LANG1, KC_BSPC, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+      _______, _______, _______, _______, _______, KC_LANG1, KC_BSPC, _______, _______, _______, _______, KC_VOLD, KC_VOLU, _______ \
       ),
 
   /* Adjust (Lower + Raise)
