ddcbee9cfc9fd72a8c44d953ef9e4d2a77eda5ae
Junya Ogura <juno@sooey.com>
2018-08-25 22:07:22 +0900

    Assign mouse cursor and click in Raise layer

--- keyboards/helix/pico/keymaps/juno/keymap.c
+++ keyboards/helix/pico/keymaps/juno/keymap.c
@@ -127,7 +127,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
       _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,                    KC_F12,  _______, KC_PSCR, KC_PGDN, KC_PGUP, _______, \
-      _______, _______, _______, _______, _______, KC_LANG1, KC_BSPC, _______, _______, _______, _______, KC_VOLD, KC_VOLU, _______ \
+      KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN1, KC_LANG1, KC_BSPC, _______, _______, _______, _______, KC_VOLD, KC_VOLU, _______ \
       ),
 
   /* Adjust (Lower + Raise)
