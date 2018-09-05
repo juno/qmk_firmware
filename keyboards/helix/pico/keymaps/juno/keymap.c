da2dff666afde12e4e03bed24a4b6aa8fb0c30ac
Junya Ogura <juno@sooey.com>
2018-09-06 02:29:52 +0900

    Update keybap for Adjust

--- keyboards/helix/pico/keymaps/juno/keymap.c
+++ keyboards/helix/pico/keymaps/juno/keymap.c
@@ -56,30 +56,31 @@ enum macro_keycodes {
 #define GUI   GUI_T(KC_ESC)         // Tap to ESC, Hold to GUI (Command)
 #define LOWER LT(_LOWER, KC_LANG2)  // Tap to 英数, Hold to Lower
 #define RAISE LT(_RAISE, KC_LANG1)  // Tap to かな, Hold to Raise
+#define TABADJ LT(_ADJUST, KC_TAB)  // Tap to TAB, Hold to Adjust
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty
    * ,-----------------------------------------.             ,-----------------------------------------.
-   * | Tab  |   Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  | Bspc |
+   * | TabAd|   Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  | Bspc |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | CTRL |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  '   |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * |Shift |   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |   /  |Shift |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * |Adjust|      |      |      |GUI/Es|Low/Ei|Space |Enter |Rai/Kn|Alt/Tb|      |      |      |      |
+   * |      |      |      |      |GUI/Es|Low/Ei|Space |Enter |Rai/Kn|Alt/Tb|      |      |      |      |
    * `-------------------------------------------------------------------------------------------------'
    */
    [_QWERTY] = LAYOUT( \
-      KC_TAB,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                  KC_Y,  KC_U,  KC_I,    KC_O,   KC_P,    KC_BSPC, \
+      TABADJ,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                  KC_Y,  KC_U,  KC_I,    KC_O,   KC_P,    KC_BSPC, \
       KC_LCTL, KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                  KC_H,  KC_J,  KC_K,    KC_L,   KC_SCLN, KC_QUOT, \
       KC_LSFT, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,                  KC_N,  KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSFT , \
-      ADJUST,  XXXXX, XXXXX, XXXXX, GUI,   LOWER, KC_SPC, KC_ENT, RAISE, ALTAB, XXXXX,   XXXXX,  XXXXX,   XXXXX \
+      XXXXX,   XXXXX, XXXXX, XXXXX, GUI,   LOWER, KC_SPC, KC_ENT, RAISE, ALTAB, XXXXX,   XXXXX,  XXXXX,   XXXXX \
       ),
 
   /* Dvorak
    * ,-----------------------------------------.             ,-----------------------------------------.
-   * | Tab  |   '  |   ,  |   .  |   P  |   Y  |             |   F  |   G  |   C  |   R  |   L  | Bspc |
+   * | TabAd|   '  |   ,  |   .  |   P  |   Y  |             |   F  |   G  |   C  |   R  |   L  | Bspc |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | CTRL |   A  |   O  |   E  |   U  |   I  |             |   D  |   H  |   T  |   N  |   S  |  /   |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
@@ -89,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_DVORAK] = LAYOUT( \
-      KC_TAB,  KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y,                  KC_F,  KC_G,  KC_C,  KC_R,  KC_L,  KC_BSPC, \
+      TABADJ,  KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y,                  KC_F,  KC_G,  KC_C,  KC_R,  KC_L,  KC_BSPC, \
       KC_LCTL, KC_A,    KC_O,    KC_E,   KC_U, KC_I,                  KC_D,  KC_H,  KC_T,  KC_N,  KC_S,  KC_SLSH, \
       KC_LSFT, KC_SCLN, KC_Q,    KC_J,   KC_K, KC_X,                  KC_B,  KC_M,  KC_W,  KC_V,  KC_Z,  KC_RSFT, \
       ADJUST,  XXXXX,   XXXXX,   XXXXX,  GUI,  LOWER, KC_SPC, KC_ENT, RAISE, ALTAB, XXXXX, XXXXX, XXXXX, XXXXX \
