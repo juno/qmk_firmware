7dbaca2e1df8ebcabb45c3ff09264083555a5ec7
Junya Ogura <juno@sooey.com>
2018-12-05 11:11:39 +0900

    Reduce TAPPING_TERM

--- keyboards/crkbd/keymaps/juno/config.h
+++ keyboards/crkbd/keymaps/juno/config.h
@@ -34,7 +34,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define PERMISSIVE_HOLD
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 250
+#define TAPPING_TERM 100
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
