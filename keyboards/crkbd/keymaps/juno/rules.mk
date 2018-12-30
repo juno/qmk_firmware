f97b96fe58c72af5be92d0fed78b547f55ad1912
Junya Ogura <juno@sooey.com>
2018-12-31 00:28:04 +0900

    Cosmetic

--- keyboards/crkbd/keymaps/juno/rules.mk
+++ keyboards/crkbd/keymaps/juno/rules.mk
@@ -2,21 +2,49 @@
 #   change to "no" to disable the options, or define them in the Makefile in
 #   the appropriate keymap folder that will get included automatically
 #
-AUDIO_ENABLE = no       # Audio output on port C6
-BACKLIGHT_ENABLE = no   # Enable keyboard backlight functionality
-BLUETOOTH_ENABLE = no   # Enable Bluetooth with the Adafruit EZ-Key HID
-BOOTMAGIC_ENABLE = no   # Virtual DIP switch configuration(+1000)
-COMMAND_ENABLE = no     # Commands for debug and configuration
-CONSOLE_ENABLE = no     # Console for debug(+400)
-EXTRAKEY_ENABLE = no    # Audio control and System control(+450)
-MIDI_ENABLE = no        # MIDI controls
-MOUSEKEY_ENABLE = no    # Mouse keys(+4700)
-NKRO_ENABLE = no        # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-RGBLIGHT_ENABLE = yes   # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
+
+# Audio output on port C6
+AUDIO_ENABLE = no
+
+# Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = no
+
+# Enable Bluetooth with the Adafruit EZ-Key HID
+BLUETOOTH_ENABLE = no
+
+# Virtual DIP switch configuration(+1000)
+BOOTMAGIC_ENABLE = no
+
+# Commands for debug and configuration
+COMMAND_ENABLE = no
+
+# Console for debug(+400)
+CONSOLE_ENABLE = no
+
+# Audio control and System control(+450)
+EXTRAKEY_ENABLE = no
+
+# MIDI controls
+MIDI_ENABLE = no
+
+# Mouse keys(+4700)
+MOUSEKEY_ENABLE = no
+
+# Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = no
+
+# Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
+RGBLIGHT_ENABLE = yes
+
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no   # Breathing sleep LED during USB suspend
-SWAP_HANDS_ENABLE = no  # Enable one-hand typing
-UNICODE_ENABLE = no     # Unicode
+
+# Enable one-hand typing
+SWAP_HANDS_ENABLE = no
+
+# Unicode
+UNICODE_ENABLE = no
+
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/glcdfont.c \
