44516fc9e5c40df34c739037f596a1aebb79ba9c
KaedeAsatsuki <flamberge@naiads.net>
2019-03-17 13:46:37 +0900

    keyboard/nirvana add

---
+++ keyboards/nirvana/nirvana.h
@@ -0,0 +1,11 @@
+#ifndef NIRVANA_H
+#define NIRVANA_H
+
+#ifdef KEYBOARD_nirvana_rev1
+    #include "rev1.h"
+#endif
+
+
+#include "quantum.h"
+
+#endif
---
+++ keyboards/nirvana/rev1/rev1.c
@@ -0,0 +1,6 @@
+#include "nirvana.h"
+
+void matrix_init_kb(void) {
+	matrix_init_user();
+};
+
